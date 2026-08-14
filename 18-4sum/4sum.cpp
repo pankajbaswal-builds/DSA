e57cddee5317044dba1a7fac8cbe35b84b84a85a
PANKAJ KUMAR BASWAL <pankajkumaarbaswal@gmail.com>
2026-08-14 14:31:20 +0530

    Time: 15 ms (92.33%) | Memory: 17.3 MB (85.64%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -3,28 +3,27 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<vector<int>>result;
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+        for(int i=0;i<n-3;i++){
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
-            for(int j=i+1;j<nums.size();j++){
+            for(int j=i+1;j<n-2;j++){
                 if(j>i+1 && nums[j]==nums[j-1]){
                     continue;
                 }
                 int left=j+1;
-                int right=nums.size()-1;
+                int right=n-1;
                     while(left<right){
                         long long sum=(long long)nums[i]+nums[j]+nums[left]+nums[right];
                         if(sum==target){
                             result.push_back({nums[i],nums[j],nums[left],nums[right]});
-                            while(left<right && nums[left]==nums[left-1]){
-                                continue;
+                            while(left<right && nums[left]==nums[left+1]){
+                                left++;
                             }
-                            while(left<right && right<nums.size()-1 && nums[right]==nums[right+1]){
-                                continue;
+                            while(left<right && nums[right]==nums[right-1]){
+                                right--;
                             }
-                            sort(result.begin(),result.end());
-                            result.erase(unique(result.begin(),result.end()),result.end());
                             left++;
                             right--;
                         }
