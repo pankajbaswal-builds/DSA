d105cacb1ee7e1c2b103c0a5fec3802923ac4105
PANKAJ KUMAR BASWAL <pankajkumaarbaswal@gmail.com>
2026-08-22 00:04:56 +0530

    Time: 8 ms (41.80%) | Memory: 13.2 MB (52.12%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> r;
-        r['I'] = 1;
-        r['V'] = 5;
-        r['X'] = 10;
-        r['L'] = 50;
-        r['C'] = 100;
-        r['D'] = 500;
-        r['M'] = 1000;
+        unordered_map<char, int> r = {
+            {'I', 1},   {'V', 5},   {'X', 10},   {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000},
+        };
         int sum = 0;
         for (int i = 0; i < s.size(); i++) {
-            if (i + 1 < s.size() && (s[i] == 'I') && (s[i + 1] == 'V')) {
-                sum += 4;
-                i++;
-            }
-            else if (i + 1 < s.size() && (s[i] == 'I') && (s[i + 1] == 'X')) {
-                sum += 9;
-                i++;
-            }
-            else if (i + 1 < s.size() && (s[i] == 'X') && (s[i + 1] == 'L')) {
-                sum += 40;
-                i++;
-            }
-            else if (i + 1 < s.size() && (s[i] == 'X') && (s[i + 1] == 'C')) {
-                sum += 90;
-                i++;
-            }
-            else if (i + 1 < s.size() && (s[i] == 'C') && (s[i + 1] == 'D')) {
-                sum += 400;
-                i++;
-            }
-            else if (i + 1 < s.size() && (s[i] == 'C') && (s[i + 1] == 'M')) {
-                sum += 900;
-                i++;
-            } 
-            else {
+            if (i + 1 < s.size() && r[s[i]] < r[s[i + 1]]) {
+                sum -= r[s[i]];
+            } else {
                 sum += r[s[i]];
             }
         }
