2607f805d409eafea11355d68c4bbc0df6882bf4
PANKAJ KUMAR BASWAL <pankajkumaarbaswal@gmail.com>
2026-08-27 14:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (79.96%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = "";
+        for (int i = 0; i < strs[0].size(); i++) {
+            char current = strs[0][i];
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].size() || strs[j][i] != current) {
+                    return prefix;
+                }
+            }
+            prefix += current;
+        }
+        return prefix;
+    }
+};
