2c7f64df32f2f0499b06a7a88f8e104891f9ed5a
PANKAJ KUMAR BASWAL <pankajkumaarbaswal@gmail.com>
2026-08-31 14:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (89.02%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,58 +1,21 @@
 class Solution {
 public:
     bool isValid(string s) {
-        unordered_map<char, char> mp = {{'(', ')'}, {'[', ']'}, {'{', '}'}};
-        unordered_map<char, int> freq;
+        string st;
         for (char ch : s) {
-            freq[ch]++;
-        }
-        bool valid = true;
-        for (auto& p : mp) {
-            char key = p.first;
-            char value = p.second;
-            if (freq[key] != freq[value]) {
-                valid = false;
-                break;
-            }
-        }
-        if (valid) {
-            stack<char> st;
-            for (char ch : s) {
-                if (mp.find(ch) != mp.end()) {
-                    st.push(ch);
-                } else {
-                    bool isValue = false;
-                    char matchingKey;
-                    for (auto& p : mp) {
-                        if (p.second == ch) {
-                            isValue = true;
-                            matchingKey = p.first;
-                            break;
-                        }
-                    }
-                    if (!isValue) {
-                        valid = false;
-                        break;
-                    }
-                    if (st.empty()) {
-                        valid = false;
-                        break;
-                    }
-                    if (st.top() != matchingKey) {
-                        valid = false;
-                        break;
-                    }
-                    st.pop();
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push_back(ch);
+            } else {
+                if (st.empty())
+                    return false;
+                char top = st.back();
+                if ((ch == ')' && top != '(') || (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
                 }
-            }
-            if (!st.empty()) {
-                valid = false;
+                st.pop_back();
             }
         }
-        if (valid) {
-            return true;
-        } else {
-            return false;
-        }
+        return st.empty();
     }
 };
